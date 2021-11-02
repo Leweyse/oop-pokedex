64c463c63f558aea683a3cbe99b0117ad5a309e6
Leweyse <darylcastro67@gmail.com>
2021-11-02 16:42:20 +0100

    implemented new classes

--- public/view.php
+++ public/view.php
@@ -11,22 +11,22 @@
     <main>
         <section class="left">
             <?php
-                // if (isset($pokemonData)) echo leftSection($pokemonData);
-                // else echo leftSection($default);
+                if (isset($pokemon -> data)) echo leftSection($pokemon -> data);
+                else echo leftSection($default);
             ?>
         </section>
         <section class="right">
             <section class="top_right">
             <form method="post" action="">
                 <label for="pokemon">search your pokemon</label>                
-                <input id="pokemon" name="name" type="text" maxlength="20" placeholder="ID or NAME" value="<?php echo $inputValue;?>" autocomplete="off" autofocus>
+                <input id="pokemon" name="name" type="text" maxlength="20" placeholder="ID or NAME" value="<?php $inputValue;?>" autocomplete="off" autofocus>
                 <input id="run" type="submit" name="submit" value="Submit">
             </form>
             </section>
             <section class="bottom_right">
                 <section class="evolutions">
                     <?php
-                        //if ($evolutionsData) echo evolutionSection($evolutionsData);
+                        if (isset($evolPokemon -> data)) echo evolutionSection($evolPokemon -> data);
                     ?>
                 </section>
                 <section class="varieties"></section>
